7dc0494b1e0edb91debefb4e5f8123fece7b81dc
김영훈 <joseph0926@naver.com>
2022-07-23 10:48:57 +0900

    ch08,09

--- ch08/src/components/Tset/Modal.js
+++ ch08/src/components/Tset/Modal.js
@@ -1,34 +1,33 @@
 import React, { Fragment, useState } from "react";
+import ReactDOM from "react-dom"
 import styles from "./Modal.module.css";
 
 const Modal = (props) => {
   const Backdrop = (props) => {
-    return <div className={styles.backdrop} onClick={props.onConfirm} />
-  }
+    return <div className={styles.backdrop} onClick={props.onConfirm} />;
+  };
 
   const ModalOverlay = (props) => {
     return (
-      <div className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{props.message}</p>
+        <div className={styles.modal}>
+          <header className={styles.header}>
+            <h2>{props.title}</h2>
+          </header>
+          <div className={styles.content}>
+            <p>{props.message}</p>
+          </div>
+          <footer className={styles.actions}>
+            <button onClick={props.onConfirm}>Okay</button>
+          </footer>
         </div>
-        <footer className={styles.actions}>
-          <button onClick={props.onConfirm}>Okay</button>
-        </footer>
-      </div>
-    )
-  }
+    );
+  };
 
   return (
     <Fragment>
-      <Backdrop>
-        <ModalOverlay></ModalOverlay>
-      </Backdrop>
+      {ReactDOM.createPortal(<Backdrop onConfirm={props.onConfirm}></Backdrop>, document.getElementById("backdrop-root"))}
+      {ReactDOM.createPortal(<ModalOverlay title={props.title} message={props.message} onConfirm={props.onConfirm}></ModalOverlay>, document.getElementById("modal-root"))}
     </Fragment>
-  );
-};
-
+  )
+}
 export default Modal;
--- ch08/src/components/Tset/Test.js
+++ ch08/src/components/Tset/Test.js
@@ -1,16 +1,34 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useState, useRef } from 'react'
 import style from "./Test.module.css"
 import Modal from './Modal';
 
 const Test = () => {
 
+  const nameInput = useRef();
+  const ageInput = useRef();
+
   const [name, setName] = useState("");
   const [age, setAge] = useState("");
   const [users, setUsers] = useState([]);
+  const [error, setError] = useState();
 
   const submitHandler = (event) => {
     event.preventDefault();
 
+    if (name.trim().length === 0 || age.trim().length === 0) {
+      setError({
+        title: "경고: 입력칸이 비었습니다",
+        message: "이름과 나이를 입력해주세요"
+      });
+      return;
+    }
+    if (parseInt(age) < 1) {
+      setError({
+        title: "경고: 음수",
+        message: "나이는 1 이상이어야 합니다"
+      });
+      return;
+    }
     if (name && age) {
       const users = {id: Math.random(), name, age}
       setUsers((user) => {
@@ -18,8 +36,6 @@ const Test = () => {
       });
       setName("");
       setAge("");
-    }
-    else if (!name || !age) {
     }
   }
 
@@ -30,16 +46,21 @@ const Test = () => {
     setAge(event.target.value);
   }
 
+  const errorHandler = () => {
+    setError(null);
+  }
+
   return (
     <Fragment>
+      {error && <Modal title={error.title} message={error.message} onConfirm={errorHandler}></Modal>}
       <form onSubmit={submitHandler} className={style.form}>
         <div className={style["input-container"]}>
           <label htmlFor='username'>Username</label>
-          <input type="text" className={style.input} id="username" name="user-name" value={name} onChange={nameHandler}></input>
+          <input type="text" className={style.input} id="username" name="user-name" value={name} onChange={nameHandler} ref={nameInput}></input>
         </div>
         <div className={style["input-container"]}>
           <label htmlFor='userage'>Age</label>
-          <input type="number" className={style.input} id="userage" name="user-age" value={age} onChange={ageHandler}></input>
+          <input type="number" className={style.input} id="userage" name="user-age" value={age} onChange={ageHandler} ref={ageInput}></input>
         </div>
         <button className={style.btn}>Add User</button>
       </form>
